df5b7926a161234d5695eac669347e9afe23e2c2
jivoi71 <jivoi71@gmail.com>
2020-08-30 20:24:56 +0300

    added_lesson_16

---
+++ Homework/lesson 16/assets/js/script.js
@@ -0,0 +1,23 @@
+/*General, trying to do something*/
+console.log('LOG: Hello World!');
+console.log('-----------------');
+
+
+/* S=lenght*width */
+let lenght = 7;
+let width = 3;
+
+console.log('Площадь прямоугольника: ' + lenght*width);
+console.log('-----------------');
+
+/* S=PI*(r*r) */
+const PI = 3.1415;
+let rad = 10;
+
+console.log('Площадь круга: ' + rad*rad*PI);
+console.log('-----------------');
+
+/* Raising a number to a power. n=3 */
+let number = 5;
+
+console.log('Степень числа: ' + number*number*number);
