e13c0480d4b45f323befddd236ca990a8bcc503d
jivoi71 <jivoi71@gmail.com>
2020-10-01 18:52:51 +0300

    lesson_26_ready_only

---
+++ Homework/lesson 26/assets/js/scripts.js
@@ -0,0 +1,18 @@
+// buttons
+
+/*1-st btn */
+
+btn.onclick = function() {
+    alert('You already pressed on me:)');
+  };
+
+/*1-st btn/ */  
+
+
+/*2-nd btn */
+
+but.ondblclick = function() {
+    alert('You made a double click:)');
+  };
+
+/*2-nd btn/ */
